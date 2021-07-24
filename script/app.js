ccfca68b413361df66b43849c69b34a1a51929eb
new-tech <77115651+hard-tech@users.noreply.github.com>
2021-07-25 01:05:45 +0200

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,10 @@
+// function controle(form1) {
+//   var test = document.form1.input.value;
+//   alert("Vous avez tapé : " + test);
+//   }
+
+
+//   <form name="form1">
+//             <input class="text" type="text" name="input" value=""><BR>
+//             <input class="input-c" type="button" name="bouton" value="Contrôler" onclick="controle(form1)">
+//             </form>
